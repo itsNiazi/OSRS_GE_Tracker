36aa8cd366c732a6cda6b59fcb9f85740d516439
itsNiazi <niazitareqmejl@gmail.com>
2024-06-01 16:59:53 +0200

    Installed required dependencies

---
+++ components/ItemChart.js
@@ -0,0 +1,13 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+export default function ItemChart() {
+  return <></>;
+}
